52d3547bb08a3d53954ae713061e0aa275298009
tofodroid <tofodroid@gmail.com>
2024-02-26 14:21:02 -0600

    Fix guide typos. Hopefully fix a MIDI system crash. Add transmitter seeking.

--- src/main/java/io/github/tofodroid/mods/mimi/common/network/NetworkProxy.java
+++ src/main/java/io/github/tofodroid/mods/mimi/common/network/NetworkProxy.java
@@ -7,18 +7,26 @@ import net.minecraft.server.level.ServerPlayer;
 
 public abstract class NetworkProxy {
     public static void sendToServer(Object message) {
-        NetworkManager.sendToServer(message);
+        if(message != null) {
+            NetworkManager.sendToServer(message);
+        }
     }
 
     public static void sendToPlayer(Object message, ServerPlayer player) {
-        NetworkManager.sendToPlayer(message, player);
+        if(message != null) {
+            NetworkManager.sendToPlayer(message, player);
+        }
     }
 
     public static void sendToPlayer(ServerPlayer player, Object message) {
-        NetworkManager.sendToPlayer(message, player);
+        if(message != null) {
+            NetworkManager.sendToPlayer(message, player);
+        }
     }
 
     public static void sendToPlayersInRange(Object message, BlockPos sourcePos, ServerLevel worldIn, ServerPlayer excludePlayer, Double range) {
-        NetworkManager.sendToPlayersInRange(message, sourcePos, worldIn, excludePlayer, range);
+        if(message != null) {
+            NetworkManager.sendToPlayersInRange(message, sourcePos, worldIn, excludePlayer, range);
+        }
     }
 }
