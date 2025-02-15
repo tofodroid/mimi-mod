a6c4d451b00b9b4813dcae274a4eb1604cc03b4a
TofoDroid <tofodroid@gmail.com>
2025-02-15 11:02:57 -0600

    typo

--- src/main/java/io/github/tofodroid/mods/mimi/common/network/MultiMidiNotePacket.java
+++ src/main/java/io/github/tofodroid/mods/mimi/common/network/MultiMidiNotePacket.java
@@ -1,6 +1,7 @@
 package io.github.tofodroid.mods.mimi.common.network;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
